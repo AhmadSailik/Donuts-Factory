464337360e6c79c92f9f97b845431a00ce541f3f
AhmadSailik <asailik1993@gmail.com>
2021-04-04 19:25:09 +0300

    creat donuts-factory demo

---
+++ Javascript/app.js
@@ -0,0 +1,12 @@
+let userName=prompt('what\'s your name?');
+console.log(userName)
+let userAge=prompt('what\'s your age?');
+console.log(userAge)
+let userFav=prompt('what\'s your language?');
+console.log(userFav)
+let userLan=prompt('what\'s your favorite donut','CHOCOLATE,VEGAN');
+console.log(userLan)
+alert('welcome '+userName+' your age is '+userAge+' and your language is '+userFav+' and your favorite donut is '+userLan);
+
+
+
